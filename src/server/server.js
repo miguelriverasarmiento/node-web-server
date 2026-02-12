e03164aad857878d4f214ff406be7663af8cb2d0
mgars90 <miguelriverasarmiento>
2026-02-12 16:10:50 -0500

    Realizamos modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const path = require("path"); // path sirve para poder leer nuestras carpetas del proyecto
+import express from "express";
+import path from "path"; // path sirve para poder leer nuestras carpetas del proyecto
 
-const startServer = (options) => {
+export const startServer = (options) => {
   const { port, public_path = "public" } = options;
 
   const app = express();
@@ -19,8 +19,4 @@ const startServer = (options) => {
   app.listen(port, () => {
     console.log(`Escuchando en el puerto ${port}`);
   });
-};
-
-module.exports = {
-  startServer,
 };
