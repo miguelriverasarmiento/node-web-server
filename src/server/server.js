a4be940f0aedf2972ff1e5ffcd709e2c946ca684
mgars90 <miguelriverasarmiento>
2026-02-12 15:35:45 -0500

    Servidor web para proyecto red social

---
+++ src/server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path"); // path sirve para poder leer nuestras carpetas del proyecto
+
+const startServer = (options) => {
+  const { port, public_path = "public" } = options;
+
+  const app = express();
+
+  // Para poder usar middlewares se usa la palabra use (express)
+  app.use(express.static(public_path)); //contenido estaticoque ponemos disponible
+
+  app.get("/", (req, res) => {
+    const indexPath = path.join(
+      __dirname + `../../../${public_path}/index.html`,
+    );
+    res.sendFile(indexPath);
+  });
+
+  app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+  });
+};
+
+module.exports = {
+  startServer,
+};
